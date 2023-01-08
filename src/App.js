9776e0bd4ba2295f09dd01591e05ce149dc48275
Sohag Himon <sohagdev19@gmail.com>
2023-01-08 14:10:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import ImageCard from './components/ImageCard'
+const App = () => {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [term, setTerm] = useState('')
 
-function App() {
+  useEffect(() => {
+    const fetchImages = async () => {
+      const data = await fetch(
+        `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+      )
+      const images = await data.json()
+      setImages(images.hits)
+      setIsLoading(false)
+    }
+    const result = fetchImages().catch(console.error)
+    console.log(result)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      <div className='grid grid-cols-3 gap-4'>
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
