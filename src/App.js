4e739a64a0fe641f89e78d6afc51925f640ded7a
sohag himon <sohagdev19@gmail.com>
2023-01-09 19:47:07 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ImageCard from './components/ImageCard'
+import ImageSearch from './components/ImageSearch'
 const App = () => {
   const [images, setImages] = useState([])
   const [isLoading, setIsLoading] = useState(true)
@@ -16,15 +17,25 @@ const App = () => {
     }
     const result = fetchImages().catch(console.error)
     console.log(result)
-  }, [])
+  }, [term])
 
   return (
     <div className='container mx-auto'>
-      <div className='grid grid-cols-3 gap-4'>
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className='text-6xl text-center mx-auto mt-32'>
+          Images Not Found...
+        </h1>
+      )}
+      {isLoading ? (
+        <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
+      ) : (
+        <div className='grid grid-cols-3 gap-4'>
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 
 const ImageCard = ({ image }) => {
+  const tags = image.tags.split(',')
+  // console.log(tags)
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src='https://source.unsplash.com/random' alt='img' />
+      <img src={image.webformatURL} alt='img' />
       <div className='px-6 py-4'>
         <div className='font-bold text-purple-500 text-xl'>
-          Photo by John doe
+          Photo by {image.user}
         </div>
         <ul>
           <li>
-            <strong>Views: </strong> 4000
+            <strong>Views: </strong> {image.views}
           </li>
           <li>
-            <strong>Downloads: </strong> 300
+            <strong>Downloads: </strong> {image.downloads}
           </li>
           <li>
-            <strong>Likes: </strong> 400
+            <strong>Likes: </strong> {image.likes}
           </li>
         </ul>
       </div>
       <div className='px-6 py-4'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag1
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag2
-        </span>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          #tag3
-        </span>
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'
+          >
+            #{tag}
+          </span>
+        ))}
       </div>
     </div>
   )
